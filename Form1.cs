1d8dac2c49909f9820ce18b04be2a5dee05d690a
s15264 <44864109+s15264@users.noreply.github.com>
2018-11-15 13:30:36 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using ObjectPooling;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pool pool = new Pool();
+            Person person01 = pool.GetPerson();
+            Console.WriteLine("First person");
+            Person person02 = pool.GetPerson();
+            Console.WriteLine("Second person");
+        }
+    }
+}
---
+++ Pool.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using System.Collections;
+
+namespace ObjectPooling
+{
+    class Person
+    {
+        public static int Counter = 0;
+        public Person()
+        {
+            ++Counter;
+        }
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+    }
+
+
+    class Pool
+    {
+        private static int poolSize = 3;
+        private static readonly Queue  objPool = new Queue(poolSize);
+
+        public Person GetPerson()
+        {
+            Person oPerson;
+            if (Person.Counter >= poolSize && objPool.Count > 0)
+            {
+                oPerson = Retrive();
+                Console.WriteLine("retrived");
+            }
+            else
+            {
+                oPerson = GetNewPerson();
+                Console.WriteLine("created");
+            }
+
+            return oPerson;
+        }
+        private Person GetNewPerson()
+        {
+            Person person = new Person();
+            objPool.Enqueue(person);
+
+            return person;
+        }
+        protected Person Retrive()
+        {
+            Person person;
+            if (objPool.Count > 0)
+            {
+                person = (Person)objPool.Dequeue();
+                Person.Counter--;
+            }
+            else
+            {
+                person = new Person();
+            }
+
+            return person;
+        }
+    }
+
+}
